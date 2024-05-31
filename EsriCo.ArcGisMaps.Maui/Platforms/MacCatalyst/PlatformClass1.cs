51bc4452a437818e4999f54d02a28d6bcd67d83c
Marcelo Cesar Torres <mtorres@esri.co>
2024-05-31 12:41:17 -0500

    Agregar archivos de proyecto.

---
+++ EsriCo.ArcGisMaps.Maui/Platforms/MacCatalyst/PlatformClass1.cs
@@ -0,0 +1,7 @@
+﻿namespace EsriCo.ArcGisMaps.Maui
+{
+  // All the code in this file is only included on Mac Catalyst.
+  public class PlatformClass1
+  {
+  }
+}
---
+++ EsriCo.ArcGisMaps.Maui/Platforms/Windows/PlatformClass1.cs
@@ -0,0 +1,7 @@
+﻿namespace EsriCo.ArcGisMaps.Maui
+{
+  // All the code in this file is only included on Windows.
+  public class PlatformClass1
+  {
+  }
+}
